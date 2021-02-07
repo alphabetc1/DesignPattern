21c080b4a82bed9e9dc6955d341142324ea9ce54
王书文 <wangshuwen.rhett@bytedance.com>
2021-02-07 15:36:55 +0800

    update builder

--- builder/client.go
+++ builder/client.go
@@ -3,14 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	thin := PersonThin{"ShuFen"}
-	fat := PersonFat{"LikeFlower"}
-
-	directorThin := PersonDirector{&thin}
-	directorThin.CreatePerson()
+	thinPerson := PersonDirector{&PersonThin{"ShuFen"}}
+	thinPerson.CreatePerson()
 
 	fmt.Println()
 
-	directorFat := PersonDirector{&fat}
-	directorFat.CreatePerson()
+	fatPerson := PersonDirector{&PersonFat{"LikeFlower"}}
+	fatPerson.CreatePerson()
 }
